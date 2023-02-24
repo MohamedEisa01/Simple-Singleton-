d186fc6d684e2c8dad7fc991c4d0ae7ebe3d8da2
mamen <mamen@ECE000065>
2023-02-25 00:08:51 +0200

    Add project files.

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private Singleton()
+        {
+            
+        }
+
+        private static Singleton _instance;
+
+        public static Singleton getInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
