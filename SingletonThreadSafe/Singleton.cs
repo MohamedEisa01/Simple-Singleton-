49dc53ed833fb7f869c0717ff831b8c30d4fea5c
mamen <mamen@ECE000065>
2023-02-25 00:17:31 +0200

    Add Singleton Thread Safe

---
+++ SingletonThreadSafe/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace SingletonThreadSafe
+{
+    public sealed class Singleton
+    {
+        private Singleton() { }
+        private static readonly object _lock = new object();
+        private static Singleton _instance = null;
+
+        public static Singleton getInstance()
+        {
+            lock (_lock)
+            {
+
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+
+
+            }
+            return _instance;
+        }
+    }
+}
